a6f9c30aa786b75b20cf96a357fa03edaf11292e
chenguanhui <chenguanhui@moxigame.cn>
2019-10-15 12:47:01 +0800

    工程初始化设置： 添加编译gulp

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const gulp = require('gulp');
+const rollup = require('rollup');
+
+
+const typescript = require('rollup-plugin-typescript2');//typescript2 plugin
+const glsl = require('rollup-plugin-glsl');
+
+
+// const ts = require('gulp-typescript');
+// let tsProject = ts.createProject('./tsconfig.json');
+// gulp.task('tsc', () =>{
+//     console.log(tsProject.config.include);
+//     gulp.src(tsProject.config.include)
+//         .pipe(tsProject())
+//         .pipe(gulp.dest(tsProject.config.compilerOptions.outDir));
+// });
+
+// gulp.task('default', ['tsc'], ()=>{
+//     gulp.watch('./src/**/*.ts', ['tsc']);
+// })
+
+
+gulp.task('compile', ()=>{
+    return rollup.rollup({
+		input: './src/Main.ts',
+		treeshake: true,//建议忽略
+		plugins: [
+			typescript({
+				check: false, //Set to false to avoid doing any diagnostic checks on the code
+				tsconfigOverride:{compilerOptions:{removeComments: true}}
+			}),
+			glsl({
+				// By default, everything gets included
+				include: /.*(.glsl|.vs|.fs)$/,
+				sourceMap: false,
+				compress:false
+			}),      
+		]
+	}).then(bundle => {
+		return bundle.write({
+			file: './bin/js/bundle.js',
+			format: 'iife',
+			name: 'laya',
+			sourcemap: false
+		});
+	});
+})
---
+++ src/base/engine/MainComponnet.ts
@@ -0,0 +1,28 @@
+export class MainComponent{
+
+    constructor(){
+
+    }
+
+    public start() : void{
+
+    }
+
+    public stop() : void{
+
+    }
+
+    public run() : void{
+
+    }
+
+    public cleanup() : void{
+
+    }
+
+    public render() : void{
+        
+    }
+
+
+}
