a478650eb409c9f47d75937188a32140b817a715
eldn <eldn000@gmail.com>
2019-10-26 14:23:04 +0800

    修改接口

--- src/engines/core/math/Vector3.ts
+++ src/engines/core/math/Vector3.ts
@@ -104,10 +104,11 @@ let _z: number = 0.0;
     }
 
  
-    public copyFrom( vector: Vector3 ): void {
+    public copyFrom( vector: Vector3 ): Vector3 {
         this._x = vector._x;
         this._y = vector._y;
         this._z = vector._z;
+        return this;
     }
 
   
