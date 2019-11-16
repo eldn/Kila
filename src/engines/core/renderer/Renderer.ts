312af9cba659311e25c3e1c350bc05c0536acb58
eldn <eldn000@gmail.com>
2019-11-16 15:27:41 +0800

    添加framebuffer测试开关

--- src/engines/core/renderer/Renderer.ts
+++ src/engines/core/renderer/Renderer.ts
@@ -11,6 +11,9 @@ import { FboTestShader } from "../gl/shaders/FBOTestShader";
 import { GLBuffer } from "../gl/GLBuffer";
 import { globalAgent } from "http";
 import { AttributeInfo } from "../gl/AttributeInfo";
+import { SharpeningShader } from "../gl/shaders/SharpeningShader";
+import { BlurShader } from "../gl/shaders/BlurShader";
+import { EdgeDetectionShader } from "../gl/shaders/EdgeDetectionShader";
 
 export class Renderer {
 
@@ -45,38 +48,39 @@ export class Renderer {
 
 
 
-    private frameBufferWidh : number = 1024;
-    private freamBufferHeight : number = 1024;
+    private testFrameBuffer : boolean = false;
+    private frameBufferWidh: number = 1024;
+    private freamBufferHeight: number = 1024;
     private framebuffer: WebGLFramebuffer;
-    private renderbuffer :WebGLRenderbuffer;
-    private textureBuffer : WebGLTexture;
+    private renderbuffer: WebGLRenderbuffer;
+    private textureBuffer: WebGLTexture;
 
-    private fboShader : FboTestShader;
-    private testVerticesBuffer : GLBuffer;
+    private fboShader: BlurShader;
+    private testVerticesBuffer: GLBuffer;
 
-    private initFramebufferObject() : void{
+    private initFramebufferObject(): void {
         if (!this.framebuffer && !this.renderbuffer) {
 
-             //定义错误函数
+            //定义错误函数
             function error() {
-                if(this.framebuffer) gl.deleteFramebuffer(this.framebuffer);
-                if(this.textureBuffer) gl.deleteFramebuffer(this.textureBuffer);
-                if(this.framebuffer) gl.deleteFramebuffer(this.framebuffer);
+                if (this.framebuffer) gl.deleteFramebuffer(this.framebuffer);
+                if (this.textureBuffer) gl.deleteFramebuffer(this.textureBuffer);
+                if (this.framebuffer) gl.deleteFramebuffer(this.framebuffer);
                 return null;
             }
 
             // 创建帧缓冲
             this.framebuffer = gl.createFramebuffer();
-            if(!this.framebuffer){
+            if (!this.framebuffer) {
                 console.log("无法创建帧缓冲区对象");
                 return error();
             }
-            
+
 
             // 把纹理对象作为缓冲颜色附件
             // 创建以及设置纹理对象
             this.textureBuffer = gl.createTexture();
-            if(!this.textureBuffer){
+            if (!this.textureBuffer) {
                 console.log("无法创建纹理对象");
                 return error();
             }
@@ -85,12 +89,12 @@ export class Renderer {
             gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, this.frameBufferWidh, this.freamBufferHeight, 0, gl.RGBA, gl.UNSIGNED_BYTE, null);
             gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
             // gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-            
+
 
             // 构造深度和模板缓冲区
             // 创建renderbuffer objects
             this.renderbuffer = gl.createRenderbuffer();
-            if(!this.renderbuffer){
+            if (!this.renderbuffer) {
                 console.log("无法创建渲染缓冲区对象");
                 return error();
             }
@@ -109,28 +113,28 @@ export class Renderer {
                 console.error("ERROR::FRAMEBUFFER:: Framebuffer is not complete!");
             }
 
-             // 解除绑定
+            // 解除绑定
             gl.bindTexture(gl.TEXTURE_2D, undefined);
             gl.bindFramebuffer(gl.FRAMEBUFFER, undefined);
             gl.bindRenderbuffer(gl.RENDERBUFFER, undefined);
         }
 
 
-        if(!this.testVerticesBuffer){
+        if (!this.testVerticesBuffer) {
 
-            let quadVertices : number[] = [   // Vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
+            let quadVertices: number[] = [   // Vertex attributes for a quad that fills the entire screen in Normalized Device Coordinates.
                 // Positions   // TexCoords
-                -1.0,  1.0,  0.0, 1.0,
-                -1.0, -1.0,  0.0, 0.0,
-                1.0, -1.0,  1.0, 0.0,
-        
-                -1.0,  1.0,  0.0, 1.0,
-                1.0, -1.0,  1.0, 0.0,
-                1.0,  1.0,  1.0, 1.0
-            ];	
+                -1.0, 1.0, 0.0, 1.0,
+                -1.0, -1.0, 0.0, 0.0,
+                1.0, -1.0, 1.0, 0.0,
+
+                -1.0, 1.0, 0.0, 1.0,
+                1.0, -1.0, 1.0, 0.0,
+                1.0, 1.0, 1.0, 1.0
+            ];
             this.testVerticesBuffer = new GLBuffer(gl.FLOAT, gl.ARRAY_BUFFER, gl.TRIANGLES);
 
-          
+
             let positionAttribute = new AttributeInfo();
             positionAttribute.location = 0;
             positionAttribute.size = 2;
@@ -146,8 +150,8 @@ export class Renderer {
             this.testVerticesBuffer.unbind();
         }
 
-        if(!this.fboShader){
-            this.fboShader = new FboTestShader();
+        if (!this.fboShader) {
+            this.fboShader = new BlurShader();
         }
 
     }
@@ -158,17 +162,14 @@ export class Renderer {
 
         // ==============> framebuff
 
-        /*
-        this.initFramebufferObject();
-
-        // 绑定帧缓冲
-        gl.viewport(Renderer.windowViewport.x, Renderer.windowViewport.y, this.frameBufferWidh, this.freamBufferHeight);
-        gl.bindFramebuffer(gl.FRAMEBUFFER, this.framebuffer);
-        gl.clearColor(0.1, 0.1, 0.1, 1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-        gl.enable(gl.DEPTH_TEST);
-        */
-
+        if (this.testFrameBuffer) {
+            this.initFramebufferObject();
+            gl.viewport(Renderer.windowViewport.x, Renderer.windowViewport.y, this.frameBufferWidh, this.freamBufferHeight);
+            gl.bindFramebuffer(gl.FRAMEBUFFER, this.framebuffer);
+            gl.clearColor(0.1, 0.1, 0.1, 1.0);
+            gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+            gl.enable(gl.DEPTH_TEST);
+        }
         // ==============> framebuff
 
 
@@ -176,30 +177,32 @@ export class Renderer {
 
     public EndRender(): void {
 
-        /*
+
         // ======> framebuffer test 
-        gl.viewport(Renderer.windowViewport.x, Renderer.windowViewport.y, Renderer.windowViewport.width, Renderer.windowViewport.height);
-        gl.bindFramebuffer(gl.FRAMEBUFFER, undefined);
-        gl.clearColor(1.0, 1.0, 1.0, 1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT);
-        gl.disable(gl.DEPTH_TEST)
+        if (this.testFrameBuffer) {
+            gl.viewport(Renderer.windowViewport.x, Renderer.windowViewport.y, Renderer.windowViewport.width, Renderer.windowViewport.height);
+            gl.bindFramebuffer(gl.FRAMEBUFFER, undefined);
+            gl.clearColor(1.0, 1.0, 1.0, 1.0);
+            gl.clear(gl.COLOR_BUFFER_BIT);
+            gl.disable(gl.DEPTH_TEST)
 
 
-        // draw framebuffer texture
-       this.fboShader.use();
-        gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D, this.textureBuffer);
-        this.fboShader.setUniform1i("screenTexture", 0);
-        this.testVerticesBuffer.bind();
-        this.testVerticesBuffer.draw();
+            // draw framebuffer texture
+            this.fboShader.use();
+            gl.activeTexture(gl.TEXTURE0);
+            gl.bindTexture(gl.TEXTURE_2D, this.textureBuffer);
+            this.fboShader.setUniform1i("screenTexture", 0);
+            this.testVerticesBuffer.bind();
+            this.testVerticesBuffer.draw();
 
 
-        // 删除帧缓冲对象
-        // gl.deleteFramebuffer(this.framebuffer);
-        // gl.deleteRenderbuffer(this.renderbuffer);
+            // 删除帧缓冲对象
+            // gl.deleteFramebuffer(this.framebuffer);
+            // gl.deleteRenderbuffer(this.renderbuffer);
+        }
 
-         // ======> framebuffer test
-         */
+        // ======> framebuffer test
+
 
 
         // Set uniforms.
