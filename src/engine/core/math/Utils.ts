8c59bd5562f73bec012086eefecb7527188f7cdb
chenguanhui <chenguanhui@moxigame.cn>
2020-04-29 20:31:08 +0800

    添加geometrydata

--- src/engine/core/math/Utils.ts
+++ src/engine/core/math/Utils.ts
@@ -1,3 +1,5 @@
+import { gl } from "../gl/GLUtilities";
+
 export const EPSILON = 0.000001;
 
 
@@ -18,24 +20,52 @@ export class Utils {
     }
 
 
-    public static getTypedArray(type: string, len: number) {
+    public static getTypedArray(type: number, len: number) {
 
         switch (type) {
-            case 'BYTE':
+            case gl.BYTE:
                 return new Int8Array(len);
-            case 'UNSIGNED_BYTE':
+            case gl.UNSIGNED_BYTE:
                 return new Uint8Array(len);
-            case 'SHORT':
+            case gl.SHORT:
                 return new Int16Array(len);
-            case 'UNSIGNED_SHORT':
+            case gl.UNSIGNED_SHORT:
                 return new Uint16Array(len);
-            case 'UNSIGNED_INT':
+            case gl.UNSIGNED_INT:
                 return new Uint32Array(len);
-            case 'FLOAT':
+            case gl.FLOAT:
                 return new Float32Array(len);
             default:
                 return new Float32Array(len);
         }
+    }
+
+    public static getTypedArrayGLType(array) {
+        if (array instanceof Float32Array) {
+            return gl.FLOAT;
+        }
+    
+        if (array instanceof Int8Array) {
+            return gl.BYTE;
+        }
+    
+        if (array instanceof Uint8Array) {
+            return gl.UNSIGNED_BYTE;
+        }
+    
+        if (array instanceof Int16Array) {
+            return gl.SHORT;
+        }
+    
+        if (array instanceof Uint16Array) {
+            return gl.UNSIGNED_SHORT;
+        }
+    
+        if (array instanceof Uint32Array) {
+            return gl.UNSIGNED_INT;
+        }
+    
+        return gl.FLOAT;
     }
 
 }
