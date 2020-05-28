d780888b3da17909b6ed4a7790ce5c46fbd9cf51
chenguanhui <chenguanhui@moxigame.cn>
2020-05-28 20:20:44 +0800

    format vector3 comment to typedoc style.

--- src/math/Vector3.ts
+++ src/math/Vector3.ts
@@ -1,91 +1,98 @@
 import { vec3 } from "gl-matrix";
 import { Quaternion } from "./Quaternion";
+import { Matrix3 } from "./Matrix3";
+import { Matrix4 } from "./Matrix4";
+import { Vector3Notifier } from "./Vector3Notifier";
 
 
  export class Vector3 {
 
+    /**
+     * 数据
+     */
     public elements : vec3;
 
     /**
      * Creates a new empty vec3
-     * @param {Number} [x=0] X component
-     * @param {Number} [y=0] Y component
-     * @param {Number} [z=0] Z component
-     * @constructs
+     * @param x X component
+     * @param y Y component
+     * @param z Z component
      */
-    constructor(x = 0, y = 0, z = 0) {
-        /**
-         * 数据
-         * @type {Float32Array}
-         */
+    constructor(x : number = 0, y : number = 0, z : number = 0) {
+       
         this.elements = vec3.fromValues(x, y, z);
     }
 
-    getClassName() : string{
+    public getClassName() : string{
         return "Vector3";
     }
 
     /**
      * Copy the values from one vec3 to this
-     * @param  {Vector3} m the source vector
-     * @return {Vector3} this
+     * @param  m the source vector
+     * @return  this
      */
-    copy(v) {
+    public copy(v : Vector3) : Vector3 {
         vec3.copy(this.elements, v.elements);
         return this;
     }
+
     /**
      * Creates a new vec3 initialized with values from this vec3
-     * @return {Vector3} a new Vector3
+     * @return a new Vector3
      */
-    clone() {
+    public clone() : Vector3 {
         const elements = this.elements;
         return new Vector3(elements[0], elements[1], elements[2]);
     }
+
     /**
      * 转换到数组
-     * @param  {Array}  [array=[]] 数组
-     * @param  {Number} [offset=0] 数组偏移值
-     * @return {Array}
+     * @param  array 数组
+     * @param  offset 数组偏移值
+     * @return
      */
-    toArray(array = [], offset = 0) {
+    public toArray(array : Array<number> = [], offset : number = 0) : Array<number> {
         const elements = this.elements;
         array[0 + offset] = elements[0];
         array[1 + offset] = elements[1];
         array[2 + offset] = elements[2];
         return array;
     }
+
     /**
      * 从数组赋值
-     * @param  {Array} array  数组
-     * @param  {Number} [offset=0] 数组偏移值
-     * @return {Vector3} this
+     * @param   array  数组
+     * @param  offset 数组偏移值
+     * @return this
      */
-    fromArray(array, offset = 0) {
+    public fromArray(array : Array<number>, offset : number = 0) : Vector3 {
         const elements = this.elements;
         elements[0] = array[offset + 0];
         elements[1] = array[offset + 1];
         elements[2] = array[offset + 2];
         return this;
     }
+
     /**
      * Set the components of a vec3 to the given values
-     * @param {Number} x X component
-     * @param {Number} y Y component
-     * @param {Number} z Z component
-     * @returns {Vector3} this
+     * @param x X component
+     * @param  y Y component
+     * @param  z Z component
+     * @returns  this
      */
-    set(x, y, z) {
+    public set(x : number, y : number, z : number) : Vector3 {
         vec3.set(this.elements, x, y, z);
         return this;
     }
+
     /**
      * Adds two vec3's
-     * @param {Vector3} a
-     * @param {Vector3} [b] 如果不传，计算 this 和 a 的和
-     * @returns {Vector3} this
+     * @param  a
+     * @param b 如果不传，计算 this 和 a 的和
+     * @returns this
      */
-    add(a : Vector3, b ?: Vector3) {
+    public add(a : Vector3, b ?: Vector3) : Vector3{
         if (!b) {
             b = a;
             a = this;
@@ -93,13 +100,14 @@ import { Quaternion } from "./Quaternion";
         vec3.add(this.elements, a.elements, b.elements);
         return this;
     }
+
     /**
      * Subtracts vector b from vector a
-     * @param {Vector3} a
-     * @param {Vector3} [b] 如果不传，计算 this 和 a 的差
-     * @returns {Vector3} this
+     * @param a
+     * @param b 如果不传，计算 this 和 a 的差
+     * @returns  this
      */
-    subtract(a : Vector3, b ?: Vector3) {
+    public subtract(a : Vector3, b ?: Vector3) : Vector3 {
         if (!b) {
             b = a;
             a = this;
@@ -107,13 +115,14 @@ import { Quaternion } from "./Quaternion";
         vec3.subtract(this.elements, a.elements, b.elements);
         return this;
     }
+
     /**
      * Multiplies two vec3's
-     * @param {Vector3} a
-     * @param {Vector3} [b] 如果不传，计算 this 和 a 的积
-     * @returns {Vector3} this
+     * @param  a
+     * @param b 如果不传，计算 this 和 a 的积
+     * @returns this
      */
-    multiply(a, b) {
+    public multiply(a : Vector3, b ?: Vector3) : Vector3{
         if (!b) {
             b = a;
             a = this;
@@ -121,13 +130,14 @@ import { Quaternion } from "./Quaternion";
         vec3.multiply(this.elements, a.elements, b.elements);
         return this;
     }
+
     /**
      * Divides two vec3's
-     * @param {Vector3} a
-     * @param {Vector3} [b] 如果不传，计算 this 和 a 的商
-     * @returns {Vector3} this
+     * @param  a
+     * @param b 如果不传，计算 this 和 a 的商
+     * @returns  this
      */
-    divide(a, b) {
+    public divide(a : Vector3, b ?: Vector3) : Vector3 {
         if (!b) {
             b = a;
             a = this;
@@ -135,29 +145,32 @@ import { Quaternion } from "./Quaternion";
         vec3.divide(this.elements, a.elements, b.elements);
         return this;
     }
+
     /**
      * Math.ceil the components of this
-     * @returns {Vector3} this
+     * @returns  this
      */
-    ceil() {
+    public ceil() : Vector3 {
         vec3.ceil(this.elements, this.elements);
         return this;
     }
+
     /**
      * Math.floor the components of this
-     * @returns {Vector3} this
+     * @returns  this
      */
-    floor() {
+    public floor() : Vector3 {
         vec3.floor(this.elements, this.elements);
         return this;
     }
+
     /**
      * Returns the minimum of two vec3's
-     * @param  {Vector3} a
-     * @param  {Vector3} [b] 如果不传，计算 this 和 a 的结果
-     * @returns {Vector3} this
+     * @param   a
+     * @param  b 如果不传，计算 this 和 a 的结果
+     * @returns this
      */
-    min(a, b) {
+    public min(a : Vector3, b ?: Vector3) : Vector3 {
         if (!b) {
             b = a;
             a = this;
@@ -165,13 +178,14 @@ import { Quaternion } from "./Quaternion";
         vec3.min(this.elements, a.elements, b.elements);
         return this;
     }
+
     /**
      * Returns the maximum of two vec3's
-     * @param  {Vector3} a
-     * @param  {Vector3} [b]  如果不传，计算 this 和 a 的结果
-     * @returns {Vector3} this
+     * @param  a
+     * @param  b  如果不传，计算 this 和 a 的结果
+     * @returns this
      */
-    max(a, b) {
+    public max(a : Vector3, b ?: Vector3) : Vector3 {
         if (!b) {
             b = a;
             a = this;
@@ -179,31 +193,34 @@ import { Quaternion } from "./Quaternion";
         vec3.max(this.elements, a.elements, b.elements);
         return this;
     }
+
     /**
      * Math.round the components of this
      * @returns {Vector3} this
      */
-    round() {
+    public round() {
         vec3.round(this.elements, this.elements);
         return this;
     }
+
     /**
      * Scales this by a scalar number
-     * @param  {Number} scale amount to scale the vector by
-     * @returns {Vector3} this
+     * @param   scale amount to scale the vector by
+     * @returns this
      */
-    scale(scale) {
+    public scale(scale : number) : Vector3{
         vec3.scale(this.elements, this.elements, scale);
         return this;
     }
+
     /**
      * Adds two vec3's after scaling the second vector by a scalar value
-     * @param  {Number} scale the amount to scale the second vector by before adding
-     * @param  {Vector3} a
-     * @param  {Vector3} [b] 如果不传，计算 this 和 a 的结果
-     * @returns {Vector3} this
+     * @param  scale the amount to scale the second vector by before adding
+     * @param  a
+     * @param  b 如果不传，计算 this 和 a 的结果
+     * @returns  this
      */
-    scaleAndAdd(scale, a, b) {
+    public scaleAndAdd(scale : number, a : Vector3, b ?: Vector3) : Vector3 {
         if (!b) {
             b = a;
             a = this;
@@ -211,94 +228,103 @@ import { Quaternion } from "./Quaternion";
         vec3.scaleAndAdd(this.elements, a.elements, b.elements, scale);
         return this;
     }
+
     /**
      * Calculates the euclidian distance between two vec3's
-     * @param  {Vector3} a
-     * @param  {Vector3} [b] 如果不传，计算 this 和 a 的结果
-     * @return {Number} distance between a and b
+     * @param   a
+     * @param  b 如果不传，计算 this 和 a 的结果
+     * @return  distance between a and b
      */
-    distance(a, b) {
+    public distance(a : Vector3, b ?: Vector3) : number {
         if (!b) {
             b = a;
             a = this;
         }
         return vec3.distance(a.elements, b.elements);
     }
+
     /**
      * Calculates the squared euclidian distance between two vec3's
-     * @param  {Vector3} a
-     * @param  {Vector3} [b] 如果不传，计算 this 和 a 的结果
-     * @return {Number} squared distance between a and b
+     * @param  a
+     * @param  b 如果不传，计算 this 和 a 的结果
+     * @return squared distance between a and b
      */
-    squaredDistance(a, b) {
+    public squaredDistance(a : Vector3, b ?: Vector3) : number {
         if (!b) {
             b = a;
             a = this;
         }
         return vec3.squaredDistance(a.elements, b.elements);
     }
+
     /**
      * Calculates the length of this
-     * @return {Number} length of this
+     * @return length of this
      */
-    length() {
+    public length() : number {
         return vec3.length(this.elements);
     }
+
     /**
      * Calculates the squared length of this
-     * @return {Number} squared length of this
+     * @return  squared length of this
      */
-    squaredLength() {
+    public squaredLength() : number {
         return vec3.squaredLength(this.elements);
     }
+
     /**
      * Negates the components of this
      * @returns {Vector3} this
      */
-    negate() {
+    public negate() : Vector3 {
         vec3.negate(this.elements, this.elements);
         return this;
     }
+
     /**
      * Returns the inverse of the components of a vec3
-     * @param  {Vector3} [a=this]
-     * @returns {Vector3} this
+     * @param  a
+     * @returns  this
      */
-    inverse(a) {
+    public inverse(a : Vector3 | Vector3Notifier = this) : Vector3{
         if (!a) {
             a = this;
         }
         vec3.inverse(this.elements, a.elements);
         return this;
     }
+
     /**
      * Normalize this
      * @returns {Vector3} this
      */
-    normalize() {
+    public normalize() {
         vec3.normalize(this.elements, this.elements);
         return this;
     }
+
     /**
      * Calculates the dot product of two vec3's
-     * @param  {Vector3} a
-     * @param  {Vector3} [b] 如果不传，计算 this 和 a 的结果
-     * @return {Number}  product of a and b
+     * @param   a
+     * @param  b 如果不传，计算 this 和 a 的结果
+     * @return  product of a and b
      */
-    dot(a : Vector3, b ?: Vector3) {
+    public dot(a : Vector3, b ?: Vector3) : number {
         if (!b) {
             b = a;
             a = this;
         }
         return vec3.dot(a.elements, b.elements);
     }
+
     /**
      * Computes the cross product of two vec3's
-     * @param  {Vector2} a
-     * @param  {Vector2} [b] 如果不传，计算 this 和 a 的结果
-     * @return {Number}  cross product of a and b
+     * @param   a
+     * @param  b 如果不传，计算 this 和 a 的结果
+     * @return   cross product of a and b
      */
-    cross(a : Vector3, b ?: Vector3) {
+    public cross(a : Vector3, b ?: Vector3) : Vector3 {
         if (!b) {
             b = a;
             a = this;
@@ -306,29 +332,32 @@ import { Quaternion } from "./Quaternion";
         vec3.cross(this.elements, a.elements, b.elements);
         return this;
     }
+
     /**
      * Performs a linear interpolation between two vec3's
-     * @param  {Vector3} v
-     * @param  {Number} t interpolation amount between the two vectors
-     * @returns {Vector3} this
+     * @param  v
+     * @param  t interpolation amount between the two vectors
+     * @returns  this
      */
-    lerp(v, t) {
+    public lerp(v : Vector3, t : number) : Vector3 {
         vec3.lerp(this.elements, this.elements, v.elements, t);
         return this;
     }
+
     /**
      * Performs a hermite interpolation with two control points
-     * @param  {Vector3} a
-     * @param  {Vector3} b
-     * @param  {Vector3} c
-     * @param  {Vector3} d
-     * @param  {Number} t interpolation amount between the two inputs
-     * @return {Vector3} this
+     * @param   a
+     * @param   b
+     * @param   c
+     * @param   d
+     * @param  t interpolation amount between the two inputs
+     * @return  this
      */
-    hermite(a, b, c, d, t) {
+    public hermite(a : Vector3, b : Vector3, c : Vector3, d : Vector3, t : number) : Vector3 {
         vec3.hermite(this.elements, a.elements, b.elements, c.elements, d.elements, t);
         return this;
     }
+
     /**
      * Performs a bezier interpolation with two control points
      * @param  {Vector3} a
@@ -338,43 +367,47 @@ import { Quaternion } from "./Quaternion";
      * @param  {Number} t interpolation amount between the two inputs
      * @return {Vector3} this
      */
-    bezier(a, b, c, d, t) {
+    public bezier(a : Vector3, b : Vector3, c : Vector3, d : Vector3, t : number) : Vector3 {
         vec3.bezier(this.elements, a.elements, b.elements, c.elements, d.elements, t);
         return this;
     }
+
     /**
      * Generates a random vector with the given scale
-     * @param  {Number} [scale=1] Length of the resulting vector. If ommitted, a unit vector will be returned
-     * @returns {Vector3} this
+     * @param  scale Length of the resulting vector. If ommitted, a unit vector will be returned
+     * @returns  this
      */
-    random(scale) {
+    public random(scale : number = 1) : Vector3 {
         vec3.random(this.elements, scale);
         return this;
     }
+
     /**
      * Transforms the vec3 with a mat3
-     * @param  {Matrix3} m matrix to transform with
-     * @returns {Vector3} this
+     * @param   m matrix to transform with
+     * @returns  this
      */
-    transformMat3(m) {
+    public transformMat3(m : Matrix3) : Vector3 {
         vec3.transformMat3(this.elements, this.elements, m.elements);
         return this;
     }
+
     /**
      * Transforms the vec3 with a mat4
-     * @param  {Matrix4} m matrix to transform with
-     * @returns {Vector3} this
+     * @param   m matrix to transform with
+     * @returns this
      */
-    transformMat4(m) {
+    public transformMat4(m : Matrix4) : Vector3{
         vec3.transformMat4(this.elements, this.elements, m.elements);
         return this;
     }
+
     /**
      * Transforms the vec3 direction with a mat4
-     * @param  {Matrix4} m matrix to transform with
-     * @returns {Vector3} this
+     * @param  m matrix to transform with
+     * @returns  this
      */
-    transformDirection(m) {
+    public transformDirection(m : Matrix4) : Vector3 {
         const elements = this.elements;
         const mElements = m.elements;
         const x = elements[0];
@@ -387,71 +420,77 @@ import { Quaternion } from "./Quaternion";
 
         return this;
     }
+
     /**
      * Transforms the vec3 with a quat
-     * @param  {Quaternion} q quaternion to transform with
-     * @returns {Vector3} this
+     * @param   q quaternion to transform with
+     * @returns  this
      */
-    transformQuat(q) {
+    public transformQuat(q : Quaternion) : Vector3 {
         vec3.transformQuat(this.elements, this.elements, q.elements);
         return this;
     }
+
     /**
      * Rotate this 3D vector around the x-axis
-     * @param  {Vector3} origin The origin of the rotation
-     * @param  {Number} rotation The angle of rotation
-     * @return {Vector3} this
+     * @param   origin The origin of the rotation
+     * @param  rotation The angle of rotation
+     * @return  this
      */
-    rotateX(origin, rotation) {
+    public rotateX(origin : Vector3, rotation : number) : Vector3 {
         vec3.rotateX(this.elements, this.elements, origin.elements, rotation);
         return this;
     }
+
     /**
      * Rotate this 3D vector around the y-axis
      * @param  {Vector3} origin The origin of the rotation
      * @param  {Number} rotation The angle of rotation
      * @return {Vector3} this
      */
-    rotateY(origin, rotation) {
+    public rotateY(origin : Vector3, rotation : number) : Vector3{
         vec3.rotateY(this.elements, this.elements, origin.elements, rotation);
         return this;
     }
+
     /**
      * Rotate this 3D vector around the z-axis
-     * @param  {Vector3} origin The origin of the rotation
-     * @param  {Number} rotation The angle of rotation
-     * @return {Vector3} this
+     * @param   origin The origin of the rotation
+     * @param   rotation The angle of rotation
+     * @return this
      */
-    rotateZ(origin, rotation) {
+    public rotateZ(origin : Vector3, rotation : number) : Vector3 {
         vec3.rotateZ(this.elements, this.elements, origin.elements, rotation);
         return this;
     }
 
-    rotate(rotation : Quaternion) : Vector3{
+    public rotate(rotation : Quaternion) : Vector3{
 		let conjugate : Quaternion = rotation.conjugate();
 		let w : Quaternion = rotation.mulVec3(this).multiply(conjugate);
 		return new Vector3(w.x, w.y, w.z);
-	}
+    }
+    
     /**
      * Returns whether or not the vectors have exactly the same elements in the same position (when compared with ===)
-     * @param  {Vector3} a
-     * @param  {Vector3} [b] 如果不传，计算 this 和 a 的结果
-     * @return {Boolean} True if the vectors are equal, false otherwise.
+     * @param   a
+     * @param  b 如果不传，计算 this 和 a 的结果
+     * @return  True if the vectors are equal, false otherwise.
      */
-    exactEquals(a, b) {
+    public exactEquals(a : Vector3, b ?: Vector3) : boolean{
         if (!b) {
             b = a;
             a = this;
         }
         return vec3.exactEquals(a.elements, b.elements);
     }
+
     /**
      * Returns whether or not the vectors have approximately the same elements in the same position.
-     * @param  {Vector3} a
-     * @param  {Vector3} [b] 如果不传，计算 this 和 a 的结果
-     * @return {Boolean} True if the vectors are equal, false otherwise.
+     * @param  a
+     * @param  b 如果不传，计算 this 和 a 的结果
+     * @return  True if the vectors are equal, false otherwise.
      */
-    equals(a : Vector3, b ?: Vector3) {
+    public equals(a : Vector3, b ?: Vector3) : boolean {
         if (!b) {
             b = a;
             a = this;
@@ -469,7 +508,6 @@ import { Quaternion } from "./Quaternion";
 
     /**
      * Y component
-     * @type {Number}
      */
      public get y(): number {
         return this.elements[1];
@@ -481,7 +519,6 @@ import { Quaternion } from "./Quaternion";
 
     /**
      * Z component
-     * @type {Number}
      */
      public get z(): number {
         return this.elements[2];
@@ -491,31 +528,31 @@ import { Quaternion } from "./Quaternion";
         this.elements[2] = value;
      }
 
-    sub(a : Vector3, b ?: Vector3){
+     public sub(a : Vector3, b ?: Vector3){
         return this.subtract(a, b);
     }
 
-    mul(a, b){
+    public mul(a : Vector3, b ?: Vector3){
         return this.multiply(a, b);
     }
 
-    div(a, b){
+    public div(a : Vector3, b ?: Vector3){
         return this.divide(a, b);
     }
 
-    dist(a, b){
+    public dist(a : Vector3, b ?: Vector3){
         return this.distance(a, b);
     }
 
-    sqrDist(a, b){
+    public sqrDist(a : Vector3, b ?: Vector3){
         return this.squaredDistance(a, b);
     }
 
-    len(){
+    public len(){
         return this.length()
     }
 
-    sqrLen(){
+    public sqrLen(){
         return this.squaredLength();
     }
 }
